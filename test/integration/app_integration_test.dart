10acd9e725b6b676e93e5c1230fb737c3339ed0e
temsk3 <83193220+temsk3@users.noreply.github.com>
2025-09-19 14:56:53 +0900

    fix: fake_cloud_firestoreを使用してFirebaseモックを改善

--- test/integration/app_integration_test.dart
+++ test/integration/app_integration_test.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -9,15 +9,14 @@ import 'package:tliny/src/app.dart';
 import 'package:tliny/src/data/general_provider.dart';
 
 // Mock classes for testing
-class MockFirebaseFirestore extends Mock implements FirebaseFirestore {}
 class MockFirebaseAuth extends Mock implements FirebaseAuth {}
+
 class MockFirebaseStorage extends Mock implements FirebaseStorage {}
-class MockCollectionReference extends Mock implements CollectionReference<Map<String, dynamic>> {}
 
 void main() {
   group('App Integration Tests', () {
     late ProviderContainer container;
-    late MockFirebaseFirestore mockFirestore;
+    late FakeFirebaseFirestore fakeFirestore;
     late MockFirebaseAuth mockAuth;
     late MockFirebaseStorage mockStorage;
 
@@ -27,13 +26,13 @@ void main() {
     });
 
     setUp(() {
-      mockFirestore = MockFirebaseFirestore();
+      fakeFirestore = FakeFirebaseFirestore();
       mockAuth = MockFirebaseAuth();
       mockStorage = MockFirebaseStorage();
-      
+
       container = ProviderContainer(
         overrides: [
-          firebaseFirestoreProvider.overrideWithValue(mockFirestore),
+          firebaseFirestoreProvider.overrideWithValue(fakeFirestore),
           firebaseAuthProvider.overrideWithValue(mockAuth),
           firebaseStorageProvider.overrideWithValue(mockStorage),
         ],
